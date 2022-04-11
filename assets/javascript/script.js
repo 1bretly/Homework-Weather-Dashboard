dc354bac7b97426c744b6aa6933d6c63cf288a90
Bret Andersen <1bretly@gmail.com>
2022-04-10 19:55:04 -0500

    modified html and added some base lets to js

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+let searchButtonEl = document.getElementById("search-button");
+let cityEl = document.getElementById("enter-city")
+let clearEl = document.getElementById("clear-history")
+let nameEl = document.getElementById("city-name");
+let currentTempEl = document.getElementById("temperature");
+let currentHumidityEl = document.getElementById("humidity");
+let currentWindEl = document.getElementById("wind-speed");
+let currentUVEl = document.getElementById("UV-index");
+let historyEl = document.getElementById("history");
+let fivedayEl = document.getElementById("fiveday-header");
+let todayweatherEl = document.getElementById("today-weather");
+
+
+// Open Weather API
+const apiKey = "c1957ee73009ff71ba1fad4664bc9bee"
+
+// fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${apiKey}`);
+// .then(response => response.json());
+// .then(geoData => {
+//     return fetch('');
+// })
+//     .then(response => response.json());
+// .then(cityData => {
+//         console.log(cityData);
+//     })
+
